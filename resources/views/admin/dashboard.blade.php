6821000f2390fa61fb71e23f5487adba95188a1e
Rachele-Turtora <racheleturtora21@gmail.com>
2024-07-27 18:27:15 +0200

    [FIX] containers

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,14 +6,16 @@
 
     <div class="row d-flex flex-wrap">
         @foreach ($projects as $project)
-        <div class="col-4">
-            <div class="card p-3 m-2">
-                <div class="dash-img-container">
-                    <img src="{{asset('storage/' . $project->cover_img)}}" alt="">
+        <div class="col-lg-4 col-sm-6">
+            <a href="{{route('admin.projects.show', $project)}}">
+                <div class="card p-3 m-2">
+                    <div class="dash-img-container">
+                        <img src="{{asset('storage/' . $project->cover_img)}}" alt="">
+                    </div>
+                    <h4>{{$project->title}}</h4>
+                    <p>{{$project->description}}</p>
                 </div>
-                <h4>{{$project->title}}</h4>
-                <p>{{$project->description}}</p>
-            </div>
+            </a>
         </div>
         @endforeach
     </div>
